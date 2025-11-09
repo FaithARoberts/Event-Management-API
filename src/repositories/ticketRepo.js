c7e5b4bef512fc3beb36f9b5da95fb4310e6393e
FaithARoberts <faithroberts3005@gmail.com>
2025-11-09 18:46:33 -0500

    Create ticketRepo.js

---
+++ src/repositories/ticketRepo.js
@@ -0,0 +1,77 @@
+import prisma from '../config/db.js';
+
+//get ticket by venue id 
+export async function findTicketsByEventsId(eventId) {
+    return await prisma.ticket.findMany({
+        where: { eventId },
+        select: {
+            id: true,
+            eventId: true ,
+            userId: true,
+            admits: true,
+            isUsed: true,
+        },
+    });
+}
+
+//create new ticket
+export async function createTicket(data) {  
+    return await prisma.ticket.create({
+        data,
+        select: {
+            id: true,
+            eventId: true ,
+            userId: true,
+            admits: true,
+            isUsed: true,
+        },
+    });
+}
+
+//get ticket by id
+export async function findTicketById(id) {
+    return await prisma.ticket.findUnique({
+        where: { id },
+        select: {
+            id: true,
+            eventId: true ,
+            userId: true,
+            admits: true,
+            isUsed: true,
+        },
+    });
+}
+
+
+//update ticket by ticketId
+export async function updateTicket(id, updates) {
+    try {
+        return await prisma.ticket.update({
+            where: { id },
+            data: updates,
+            select: {
+                id: true,
+            eventId: true ,
+            userId: true,
+            admits: true,
+            isUsed: true,
+            },
+        });
+    } catch (err) {
+        if (err.code === 'P2025') return null;
+        throw err;
+    }
+}
+
+//delete ticket by id
+export async function deleteTicket(id) {
+    try {
+        await prisma.ticket.delete({
+            where: { id },
+        });
+        return true;
+    } catch (err) {
+        if (err.code === 'P2025') return null;
+        throw err;
+    }
+}
