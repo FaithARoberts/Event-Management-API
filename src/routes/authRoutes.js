12b439c00467b4b91d68296ff1b4b314d20a4c30
FaithARoberts <faithroberts3005@gmail.com>
2025-11-18 17:55:30 -0500

    User Validation & Login rate limiter
    Added user validation and login limiter middleware.
    Added Validation error handling middleware.
    Added middleware to authRoutes.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import * as authController from '../controllers/authController.js';
-import {} from '../middleware/validateAuth.js';
+import {validateLogIn, validateUser} from '../middleware/validateUser.js';
+import {handleValidationErrors} from '../middleware/handleValidationError.js';
+import logInLimiter from '../middleware/logInLimiter.js';
 
 const router = express.Router();
 
-router.post('/signup', authController.signUpHandler);
-router.post('/login', authController.logInHandler);
+router.post('/signup', validateUser, handleValidationErrors, authController.signUpHandler);
+router.post('/login', logInLimiter, validateLogIn, handleValidationErrors, authController.logInHandler);
 
 export default router;
